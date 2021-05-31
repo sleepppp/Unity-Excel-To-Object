88a9379a73f26beedb6ed1fb312bb08d84384cb0
sleepppp <32008212+sleepppp@users.noreply.github.com>
2021-05-31 14:25:40 +0900

    Editor Code

---
+++ ExcelData/Assets/Scripts/ExcelController/Data/Editor/EditorDataWindow.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+namespace Core.Data
+{
+    public class EditorDataWindow : EditorWindow
+    {
+        [MenuItem("Window/Data/ExcelData")]
+        static void Open()
+        {
+            EditorDataWindow window = 
+                EditorWindow.GetWindow(typeof(EditorDataWindow)) as EditorDataWindow;
+            if (window == null)
+                return;
+
+            window.Show();
+        }
+
+        private void OnGUI()
+        {
+            
+        }
+    }
+}
